526d61ccdc15a7fff0809cd0214da063694c3a06
alexwilkinson <alex.wilkinson@me.com>
2014-04-01 16:10:31 -0400

    scraping only on index load

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+  before_action :clear_database, :only => [:index]
+
   include BooksHelper
 
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
-    @isbn = find_isbn
+    find_books
   end
 
   def preview
@@ -25,6 +26,9 @@ class BooksController < ApplicationController
   end
 
   private
+    def clear_database
+      Book.destroy_all
+    end
 
     def book_params
       params.require(:book).permit(:title, :author, :isbn, :release_date, :excerpt)
