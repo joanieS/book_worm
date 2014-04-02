09bdfd6953fad983cb41e07d9305d4125ebc975f
Joan Soskin <joan.e.soskin@gmail.com>
2014-04-02 17:38:31 -0400

    made it pretty

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,44 +2,75 @@ require 'open-uri'
 
 module BooksHelper
 
-  URLS = "https://www.goodreads.com/book/most_read"
-  URLS.each do |url|
-    titles = find_titles(url)
-    titles.each do |title|
-      find_book(title)
+  def find_books
+    url = "https://www.goodreads.com/book/most_read"
+    page = Nokogiri::HTML(open(url)) 
+    books = page.css('div.leftContainer table.tableList tr td a.bookTitle span')
+    titles = []
+    books.each do |book|
+      Book.create(title: book.text)
+    end
   end
 
-  def find_titles(url)
-    page = Nokogiri::HTML(open(url))
-    page.css('div.leftContainer table.tableList tr td a.bookTitle span')
-  end
-
-  def find_book(title)
-    request = Typhoeus::Request.new(
-    "https://www.googleapis.com/books/v1/volumes",
-    params: { q: CGI::escape("The History of Tom Jones"), filter: "partial", key: ENV["GOOGLE_BOOKS_API_KEY"]},
-    )
-
-    response = request.run
-
-    my_stuff = JSON.parse(response.body)
-    option = my_stuff["items"].first
-    if option["volumeInfo"]["title"] == title
-      author = option["volumeInfo"]["authors"]
-      isbn = option["volumeInfo"]["authors"]
-      authors = option["volumeInfo"]["authors"]
-
-    else next
-    # if my_stuff["items"].first["volumeInfo"]["title"] != (our title variable)
-
-    # my_stuff["items"].first["volumeInfo"]["previewLink"]
-
+  def find_single_isbn(book)
+    title = book.title
+    book_info = GoogleBooks.search(title).first
+    book.isbn = book_info.isbn
+    
   end
 
   def find_isbn
+    title = Book.all.sample.title
+    book_obj = {title:"", release_date: "", isbn:"", excerpt:"", authors:""}
+    book_info = GoogleBooks.search(title, :api_key => ENV["GOOGLE_BOOKS_API_KEY"]).first
     book_obj = {title: book_info.title, release_date: book_info.published_date, isbn: book_info.isbn, excerpt:book_info.preview_link, authors:book_info.authors_array}
     book_obj[:isbn]
   end
 
-  
 end
+
+# require 'open-uri'
+
+# module BooksHelper
+
+#   URLS = "https://www.goodreads.com/book/most_read"
+#   URLS.each do |url|
+#     titles = find_titles(url)
+#     titles.each do |title|
+#       find_book(title)
+#   end
+
+#   def find_titles(url)
+#     page = Nokogiri::HTML(open(url))
+#     page.css('div.leftContainer table.tableList tr td a.bookTitle span')
+#   end
+
+#   def find_book(title)
+#     request = Typhoeus::Request.new(
+#     "https://www.googleapis.com/books/v1/volumes",
+#     params: { q: CGI::escape("The History of Tom Jones"), filter: "partial", key: ENV["GOOGLE_BOOKS_API_KEY"]},
+#     )
+
+#     response = request.run
+
+#     my_stuff = JSON.parse(response.body)
+#     option = my_stuff["items"].first
+#     if option["volumeInfo"]["title"] == title
+#       author = option["volumeInfo"]["authors"]
+#       isbn = option["volumeInfo"]["authors"]
+#       authors = option["volumeInfo"]["authors"]
+
+#     else next
+#     # if my_stuff["items"].first["volumeInfo"]["title"] != (our title variable)
+
+#     # my_stuff["items"].first["volumeInfo"]["previewLink"]
+
+#   end
+
+#   def find_isbn
+#     book_obj = {title: book_info.title, release_date: book_info.published_date, isbn: book_info.isbn, excerpt:book_info.preview_link, authors:book_info.authors_array}
+#     book_obj[:isbn]
+#   end
+
+  
+# end
