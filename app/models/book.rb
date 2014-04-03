d928e960e529968339afa823111ef7afa8225a80
alexwilkinson <alex.wilkinson@me.com>
2014-04-03 16:31:54 -0400

    added urls

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,7 +19,34 @@ class Book < ActiveRecord::Base
   end
 
   def self.save_books
-    urls = ["https://www.goodreads.com/book/most_read"]
+    urls = [
+      "https://www.goodreads.com/book/most_read",
+      "http://www.goodreads.com/list/show/3.Best_Science_Fiction_Fantasy_Books",
+      "http://www.goodreads.com/list/show/397.Best_Paranormal_Romance_Series",
+      "http://www.goodreads.com/list/show/264.Books_That_Everyone_Should_Read_At_Least_Once",
+      "http://www.goodreads.com/list/show/2602.Best_Female_Lead_Characters",
+      "http://www.goodreads.com/list/show/135.Best_Horror_Novels",
+      "http://www.goodreads.com/list/show/7205.Best_of_Little_Known_Authors",
+      "http://www.goodreads.com/list/show/50.The_Best_Epic_Fantasy",
+      "http://www.goodreads.com/list/show/8166.Books_You_Wish_More_People_Knew_About",
+      "http://www.goodreads.com/list/show/15.Best_Historical_Fiction",
+      "http://www.goodreads.com/list/show/183.Tales_of_New_York_City",
+      "http://www.goodreads.com/list/show/824.Best_Non_fiction_War_Books",
+      "http://www.goodreads.com/list/show/338.Immigrant_Experience_Literature",
+      "http://www.goodreads.com/list/show/16.Best_Books_of_the_19th_Century",
+      "https://www.goodreads.com/list/show/6",
+      "https://www.goodreads.com/list/show/7",
+      "https://www.goodreads.com/list/show/30",
+      "http://www.goodreads.com/list/show/348.Thrillers",
+      "http://www.goodreads.com/list/show/281.Best_Memoir_Biography_Autobiography",
+      "http://www.goodreads.com/list/show/735.Must_Read_Non_Fiction",
+      "http://www.goodreads.com/list/show/10925.Funny_Women_Memoirs",
+      "http://www.goodreads.com/list/show/29013.Best_Biographies",
+      "http://www.goodreads.com/list/show/11.Best_Crime_Mystery_Books",
+      "http://www.goodreads.com/list/show/36.Best_Poetry_Books",
+      "http://www.goodreads.com/list/show/495.Best_of_William_Shakespeare",
+      "http://www.goodreads.com/list/show/453.Best_Philosophical_Literature"
+    ]
     urls.each do |url|
       results = Book.find_titles(url)
       results.each do |result|
