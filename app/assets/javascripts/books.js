5311ae4f27f24b035f6e12bfd4c015d0754c3313
alexwilkinson <alex.wilkinson@me.com>
2014-04-29 11:30:25 -0400

    refactored book viewer code, index files, book js files

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  function incrementPostLikeCount(post_id,post_likes_count){
-    $("#like_post_"+post_id +" span.count").html(post_likes_count);
-  };
+  $("button#read").on("click", function(e){
+    $.getScript("/preview", function(e){
+      $("#myModal").modal('toggle', function(){});
+    });
+  });
+  $("button#next").on("click", function(e){
+      $.getScript("/preview", function(e){});
+  });
+  $("button#like").on("click", function(e){
+      $.getScript("/like"); 
+      $.getScript("/preview", function(e) {});
+  });
+  $("button#dislike").on("click", function(e){
+      $.getScript("/dislike");
+      $.getScript("/preview", function(e) {});
+  });
 });
 
---
+++ app/assets/javascripts/embed.js
@@ -0,0 +1,1 @@
+google.load("books", "0");
